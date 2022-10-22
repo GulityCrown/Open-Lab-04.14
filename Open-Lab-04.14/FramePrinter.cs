bf7570178d2b3f15c252baeba071f4a54e63e5e1
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-10-22 14:17:27 +0200

    Update FramePrinter.cs

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Linq;
+
+
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +9,47 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int longest = 0;
+            foreach (string str in strings)
+            {
+                if (str.Length > longest)
+                    longest = str.Length;
+            }
+            char[] A = new char[longest + 4];
+            char a = '*';
+            for(int i = 0; i < A.Length; i++)
+            {
+                A[i] += a;
+            }
+            Console.WriteLine(A);
+            for (int i = 0; i < strings.Length; i++)
+            {
+
+                if (strings[i].Length == A.Length - 4)
+                {
+                    Console.WriteLine("* " + strings[i] + " *");
+                }
+                else if (strings[i].Length != A.Length - 4)
+                {
+                    Console.WriteLine("* "+ strings[i]+PocetMedzier(A.Length - strings[i].Length - 4) + " *");
+                }
+
+            }
+                Console.WriteLine(A);
         }
+        public string PocetMedzier(int i)
+        {
+            string medzeri = "";
+            char c = ' ';
+
+            for(int j = 0; j< i; j++)
+            {
+                medzeri += c; 
+            }
+            return medzeri;
+            
+            
+        }
+
     }
 }
